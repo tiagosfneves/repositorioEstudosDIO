7e6e2995108866656ad3755d782fae65b23cc9af
tiagonevestec <tiagonevestec@gmail.com>
2023-05-01 03:46:04 -0300

    3 alteração, add pasta JS e conteúdo da 1 aula com desafio feito e com incremento extra

---
+++ Aulas/JS/01_Aula/primeiroDesafio.js
@@ -0,0 +1,15 @@
+/* 
+  Calcular distancia;
+  Preço do combustivel;
+  Média de consumo;
+  Media de gasto para uma viagem "X"
+*/
+
+const valorEtanol = 4.99;
+let consumoKmPorLitro = 15;
+const distanciaEmKm = 1580;
+
+const litrosConsumidos = distanciaEmKm / consumoKmPorLitro;
+const valorGastoNaViagem = litrosConsumidos * valorEtanol
+
+console.log("Total de litros gastos: ", litrosConsumidos.toFixed(2), "e Total gasto em dinheiro: ", valorGastoNaViagem.toFixed(2));
